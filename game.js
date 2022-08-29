f57735affaa7a5d80b295c6ee441f40752af2188
Nithish Krishna <105929790+nithishkishna@users.noreply.github.com>
2022-08-29 19:44:57 +0530

    Added files for the site . Act 1

---
+++ game.js
@@ -0,0 +1,78 @@
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var buttonColors = ["red","blue","green","yellow"];
+
+var level = 0;
+var started = false;
+
+$(document).keydown(function(){
+  if(!started){
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function(event){
+
+  var userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+
+});
+
+function nextSequence(){
+
+  userClickedPattern = [];
+  level += 1;
+  $("h1").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  },100);
+}
+
+function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+    if(userClickedPattern.length === gamePattern.length){
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+    }
+  }
+  else{
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  started = false;
+}
